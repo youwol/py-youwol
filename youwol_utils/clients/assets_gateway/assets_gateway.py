d78667de385adc21c3075572307269da708385a4
greinisch <greinisch@youwol.com>
2021-07-02 15:47:21 +0200

    :zap: youwol-utils, assets-gtw=>add cdn_get_versions

--- youwol_utils/clients/assets_gateway/assets_gateway.py
+++ youwol_utils/clients/assets_gateway/assets_gateway.py
@@ -177,6 +177,16 @@ class AssetsGatewayClient:
                     return await resp.read()
                 await raise_exception_from_response(resp, url=url, headers=self.headers)
 
+    async def cdn_get_versions(self, package_id: str, **kwargs):
+
+        url = f"{self.url_base}/raw/package/metadata/{package_id}"
+
+        async with aiohttp.ClientSession(connector=self.get_aiohttp_connector(), headers=self.headers) as session:
+            async with await session.get(url, **kwargs) as resp:
+                if resp.status == 200:
+                    return await resp.json()
+                await raise_exception_from_response(resp)
+
     async def cdn_loading_graph(self, body, **kwargs):
 
         url = f"{self.url_base}/cdn/queries/loading-graph"
