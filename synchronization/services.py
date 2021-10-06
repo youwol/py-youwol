8a6aef1dcc352719e4625d9d8e95cb7082f33501
greinisch <greinisch@youwol.com>
2021-10-06 11:30:15 +0200

    :arrow_up: synchronize=> add flux_backend 'backward_compatibility.py'

--- synchronization/services.py
+++ synchronization/services.py
@@ -42,7 +42,7 @@ def included_services(platform_path, open_source_path):
             src=src_backend_services / 'flux-backend' / 'src' / 'youwol_flux',
             dst=dst_services / 'backs' / 'flux',
             include=["/__init__.py", "/models.py", "/root_paths.py", "/suggestions.py", "/utils.py",
-                     "/workflow_new_project.py"]
+                     "/workflow_new_project.py", "/backward_compatibility.py"]
             ),
         ServiceInjection(
             src=src_backend_services / 'assets-gateway' / 'src' / 'youwol_assets_gateway',
