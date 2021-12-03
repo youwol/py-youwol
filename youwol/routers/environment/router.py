7585afa2dfc39f0589af4138b9933d8c6a9c2738
greinisch <greinisch@youwol.com>
2021-12-03 12:20:46 +0100

    :construction: comment auto-update code for latter recycled use

--- youwol/routers/environment/router.py
+++ youwol/routers/environment/router.py
@@ -1,9 +1,5 @@
 import asyncio
 import itertools
-import os
-import zipfile
-from io import BytesIO
-from pathlib import Path
 
 from fastapi import APIRouter, WebSocket, Depends
 from aiohttp.web import HTTPException
@@ -17,11 +13,6 @@ import youwol.services.backs.stories.configurations as stories
 import youwol.services.backs.assets.configurations as assets
 import youwol.services.backs.assets_gateway.configurations as assets_gtw
 
-import youwol.services.fronts.flux_builder as flux_builder
-import youwol.services.fronts.flux_runner as flux_runner
-import youwol.services.fronts.workspace_explorer as workspace_explorer
-from services.backs.cdn.utils import to_package_id
-
 from youwol.configuration.user_configuration import get_public_user_auth_token
 from youwol.configuration import ErrorResponse
 from youwol.configuration.youwol_configuration import YouwolConfiguration
@@ -29,8 +20,7 @@ from youwol.configuration.youwol_configuration import yw_config, YouwolConfigura
 from youwol.context import Context, ActionStep
 from youwol.routers.environment.models import (
     StatusResponse, SwitchConfigurationBody, SyncUserBody, LoginBody,
-    PostParametersBody, RemoteGatewayInfo, SelectRemoteBody, SyncComponentBody, ComponentsUpdate, ComponentUpdate,
-    ComponentsUpdateStatus,
+    PostParametersBody, RemoteGatewayInfo, SelectRemoteBody, SyncComponentBody, ComponentsUpdate
     )
 from youwol.utils_low_level import to_json, start_web_socket
 from youwol.utils_paths import parse_json, write_json
@@ -99,6 +89,7 @@ async def ws_endpoint(ws: WebSocket):
                 "firstError": to_json(first_error)
                 })
 
+    # start_thread_asset_auto_download()
     await start_web_socket(ws)
 
 
@@ -275,51 +266,52 @@ async def available_updates(
         request: Request,
         config: YouwolConfiguration = Depends(yw_config)
         ):
+    return
+"""
+pending = ComponentsUpdate(
+    components=[],
+    status=ComponentsUpdateStatus.PENDING)
 
-    pending = ComponentsUpdate(
-        components=[],
-        status=ComponentsUpdateStatus.PENDING)
+WebSocketsCache.environment and await WebSocketsCache.environment.send_json(to_json(pending))
 
-    WebSocketsCache.environment and await WebSocketsCache.environment.send_json(to_json(pending))
+context = Context(
+    request=request,
+    config=config,
+    web_socket=WebSocketsCache.environment
+    )
 
-    context = Context(
-        request=request,
-        config=config,
-        web_socket=WebSocketsCache.environment
-        )
-
-    async def get_update_description(package_name: str, local_folder: Path):
-        local_version = parse_json(local_folder / 'package.json')['version']
-        client = await config.get_assets_gateway_client(context=context)
-        resp = await client.cdn_get_versions(package_id=to_package_id(package_name))
-        sync_status = ComponentsUpdateStatus.SYNC \
-            if local_version==resp['versions'][0] \
-            else ComponentsUpdateStatus.OUTDATED
-
-        return ComponentUpdate(name=package_name, localVersion=local_version, latestVersion=resp['versions'][0],
-                               status=sync_status)
-
-    builder, runner, explorer = await asyncio.gather(
-        get_update_description(package_name="@youwol/flux-builder",
-                               local_folder=Path(flux_builder.__file__).parent),
-        get_update_description(package_name="@youwol/flux-runner",
-                               local_folder=Path(flux_runner.__file__).parent),
-        get_update_description(package_name="@youwol/workspace-explorer",
-                               local_folder=Path(workspace_explorer.__file__).parent)
-        )
-    components = [builder, runner, explorer]
-    glob_status = ComponentsUpdateStatus.SYNC \
-        if all([t.status == ComponentsUpdateStatus.SYNC for t in components]) \
+async def get_update_description(package_name: str, local_folder: Path):
+    local_version = parse_json(local_folder / 'package.json')['version']
+    client = await config.get_assets_gateway_client(context=context)
+    resp = await client.cdn_get_versions(package_id=to_package_id(package_name))
+    sync_status = ComponentsUpdateStatus.SYNC \
+        if local_version == resp['versions'][0] \
         else ComponentsUpdateStatus.OUTDATED
 
-    response = ComponentsUpdate(
-        components=[builder, runner, explorer],
-        status=glob_status)
+    return ComponentUpdate(name=package_name, localVersion=local_version, latestVersion=resp['versions'][0],
+                           status=sync_status)
 
-    WebSocketsCache.environment and await WebSocketsCache.environment.send_json(to_json(response))
+builder, runner, explorer = await asyncio.gather(
+    get_update_description(package_name="@youwol/flux-builder",
+                           local_folder=Path(flux_builder.__file__).parent),
+    get_update_description(package_name="@youwol/flux-runner",
+                           local_folder=Path(flux_runner.__file__).parent),
+    get_update_description(package_name="@youwol/workspace-explorer",
+                           local_folder=Path(workspace_explorer.__file__).parent)
+    )
+components = [builder, runner, explorer]
+glob_status = ComponentsUpdateStatus.SYNC \
+    if all([t.status == ComponentsUpdateStatus.SYNC for t in components]) \
+    else ComponentsUpdateStatus.OUTDATED
 
-    return response
+response = ComponentsUpdate(
+    components=[builder, runner, explorer],
+    status=glob_status)
 
+WebSocketsCache.environment and await WebSocketsCache.environment.send_json(to_json(response))
+
+return response
+"""
 
 @router.post("/sync-component",
              summary="Synchronise a component w/ latest version"
@@ -329,39 +321,41 @@ async def sync_component(
         body: SyncComponentBody,
         config: YouwolConfiguration = Depends(yw_config)
         ):
+    return
+"""
+pending = ComponentsUpdate(
+    components=[],
+    status=ComponentsUpdateStatus.PENDING)
 
-    pending = ComponentsUpdate(
-        components=[],
-        status=ComponentsUpdateStatus.PENDING)
+WebSocketsCache.environment and await WebSocketsCache.environment.send_json(to_json(pending))
 
-    WebSocketsCache.environment and await WebSocketsCache.environment.send_json(to_json(pending))
+folder_paths = {
+    "@youwol/flux-builder": Path(flux_builder.__file__).parent,
+    "@youwol/flux-runner": Path(flux_runner.__file__).parent,
+    "@youwol/workspace-explorer": Path(workspace_explorer.__file__).parent
+    }
+context = Context(
+    request=request,
+    config=config,
+    web_socket=WebSocketsCache.environment
+    )
+async with context.start(action=f"Synchronise {body.name}") as ctx:
+    client = await config.get_assets_gateway_client(context=context)
+    zip_bytes = await client.cdn_get_package(library_name=body.name, version=body.version)
 
-    folder_paths = {
-        "@youwol/flux-builder": Path(flux_builder.__file__).parent,
-        "@youwol/flux-runner": Path(flux_runner.__file__).parent,
-        "@youwol/workspace-explorer": Path(workspace_explorer.__file__).parent
-        }
-    context = Context(
-        request=request,
-        config=config,
-        web_socket=WebSocketsCache.environment
-        )
-    async with context.start(action=f"Synchronise {body.name}") as ctx:
-        client = await config.get_assets_gateway_client(context=context)
-        zip_bytes = await client.cdn_get_package(library_name=body.name, version=body.version)
+    zip_content = zipfile.ZipFile(BytesIO(zip_bytes))
+    to_extract = [f for f in zip_content.namelist() if f.startswith('dist/') or f == "package.json"]
+    files = [zip_content.open(filename) for filename in to_extract]
+    folder_path = folder_paths[body.name]
+    await ctx.info(step=ActionStep.STATUS, content=f'{len(to_extract)} files to copy', json={'files': to_extract})
+    old_files = [f for f in os.listdir(folder_path) if f != '__init__.py' and not (folder_path / f).is_dir()]
+    for f in old_files:
+        os.remove(folder_path / f)
 
-        zip_content = zipfile.ZipFile(BytesIO(zip_bytes))
-        to_extract = [f for f in zip_content.namelist() if f.startswith('dist/') or f == "package.json"]
-        files = [zip_content.open(filename) for filename in to_extract]
-        folder_path = folder_paths[body.name]
-        await ctx.info(step=ActionStep.STATUS, content=f'{len(to_extract)} files to copy', json={'files': to_extract})
-        old_files = [f for f in os.listdir(folder_path) if f != '__init__.py' and not (folder_path / f).is_dir()]
-        for f in old_files:
-            os.remove(folder_path / f)
-
-        for name, file_bytes in zip(to_extract, files):
-            path = folder_path / name.split('/')[-1]
-            with open(path, 'wb') as file:
-                file.write(file_bytes.read())
-        await available_updates(request=request, config=config)
-        return {}
+    for name, file_bytes in zip(to_extract, files):
+        path = folder_path / name.split('/')[-1]
+        with open(path, 'wb') as file:
+            file.write(file_bytes.read())
+    await available_updates(request=request, config=config)
+    return {}
+"""
