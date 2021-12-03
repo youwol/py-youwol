9c9a19a6147b1f0586f648009a39963a9b56a4bb
greinisch <greinisch@youwol.com>
2021-12-03 12:21:44 +0100

    :recycle: initial data => becomes mostly empty

--- youwol_data/yw_config.py
+++ youwol_data/yw_config.py
@@ -1,18 +1,14 @@
-import os
-from pathlib import Path
 from typing import Dict
+from fastapi import APIRouter
 
-from youwol.utils_paths import ensure_folders
-from youwol.configuration import (
-    BackEnds, TargetBack, ConfigParameters, parameter_enum, FrontEnds, Packages,
-    UserConfiguration, General, TargetFront,
-    )
+from configuration import Packages
+from utils_paths import ensure_folders, get_targets
+from youwol.configuration.models_base import ConfigParameters, parameter_enum
+from youwol.configuration.user_configuration import UserConfiguration, General
 from youwol.main_args import MainArguments
 
-from youwol.pipelines.fastapi.main import fast_api_pipeline
+import youwol.pipelines.library_webpack_ts.main as npm_package
 import youwol.pipelines.flux_pack.main as flux_pack
-from youwol.pipelines.scribble_html.main import scribble_html_pipeline
-import youwol.pipelines.library_webpack_ts.main as simple_ts_lib
 
 
 async def configuration_parameters():
@@ -20,7 +16,7 @@ async def configuration_parameters():
         parameters={
             "bundleMode": parameter_enum(
                 name="bundle mode",
-                value="dev",
+                value="prod",
                 values=["dev", "prod"],
                 description="Mode to bundle npm package when applicable"
                 )
@@ -28,78 +24,46 @@ async def configuration_parameters():
         )
 
 
-async def configuration(main_args: MainArguments, parameters: Dict[str, any]):
+async def configuration(_main_args: MainArguments, parameters: Dict[str, any]):
 
-    yw_config_path = Path("{{folder_path}}")
-
-    data_path, workspace_path, system_path = ensure_folders(
-        yw_config_path / "databases",
-        yw_config_path / "workspace",
-        yw_config_path / "youwol_system"
+    config_dir = _main_args.config_path.parent
+    data_path = config_dir
+    projects_path, databases_path, youwol_system_path = ensure_folders(
+        data_path / "projects",
+        data_path / "databases",
+        data_path / "youwol_system"
         )
-
-    packages_path, flux_path = ensure_folders(
-        workspace_path / "packages",
-        workspace_path / "packages" / "flux"
-        )
-
-    backends_path, python_backs, fast_api_path = ensure_folders(
-        workspace_path / "backends",
-        workspace_path / "backends" / "python",
-        workspace_path / "backends" / "python" / "fast-api"
-        )
-
-    frontends_path, scribble_path = ensure_folders(
-        workspace_path / "frontends",
-        workspace_path / "frontends" / "scribbles"
-        )
-
     return UserConfiguration(
         general=General(
-            databasesFolder=data_path,
-            secretsFile=yw_config_path / "secrets.json",
-            usersInfo=yw_config_path / "users-info.json",
-            systemFolder=system_path,
-            remotesInfo=yw_config_path / "remotes-info.json",
-            defaultPublishLocation="private/default-drive",
+            databasesFolder=databases_path,
+            systemFolder=youwol_system_path,
+            remotesInfo=config_dir / "remotes-info.json",
+            usersInfo=config_dir / "users-info.json",
+            secretsFile=config_dir / "secrets.json",
             openid_host="gc.auth.youwol.com"
             ),
         packages=Packages(
             pipelines={
+                "npm-package": npm_package.pipeline(
+                    skeleton_path=projects_path,
+                    mode=npm_package.BundleModeEnum.PROD if parameters["bundleMode"] == 'prod' else
+                    npm_package.BundleModeEnum.DEV
+                    ),
                 "flux-pack": flux_pack.pipeline(
-                    skeleton_path=flux_path,
-                    mode=flux_pack.BundleModeEnum.PROD
-                    if parameters["bundleMode"] == 'prod'
-                    else flux_pack.BundleModeEnum.DEV
-                    ),
-                "simple-pack": simple_ts_lib.pipeline(
-                    skeleton_path=packages_path,
-                    mode=simple_ts_lib.BundleModeEnum.PROD
-                    if parameters["bundleMode"] == 'prod'
-                    else simple_ts_lib.BundleModeEnum.DEV
+                    skeleton_path=projects_path,
+                    mode=flux_pack.BundleModeEnum.PROD if parameters["bundleMode"] == 'prod' else
+                    flux_pack.BundleModeEnum.DEV
                     ),
                 },
             targets={
-                "flux-pack": flux_pack.get_targets(folder=flux_path),
-                "simple-pack": simple_ts_lib.get_targets(folder=packages_path)
-                }
-            ),
-        backends=BackEnds(
-            pipelines={
-                "fast-api": fast_api_pipeline(path=fast_api_path, conf="local")
-                },
-            targets={
-                "fast-api": [TargetBack(folder=fast_api_path / f) for f in os.listdir(fast_api_path)
-                             if (fast_api_path / f / 'src' / 'main.py').exists()]
-                }
-            ),
-        frontends=FrontEnds(
-            pipelines={
-                "scratch-html": scribble_html_pipeline(path=scribble_path)
-                },
-            targets={
-                "scratch-html": [TargetFront(folder=scribble_path / f) for f in os.listdir(scribble_path)
-                                 if (scribble_path / f / 'index.html').exists()]
+                "npm-package": get_targets(
+                    folders=[projects_path],
+                    pipeline_name='yw_pipeline_webpack_ts'),
+                "flux-pack": get_targets(
+                    folders=[projects_path],
+                    pipeline_name='yw_pipeline_flux_pack')
                 }
             )
         )
+
+router = APIRouter()
