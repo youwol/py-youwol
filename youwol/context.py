831b040e503cf1281f6d740fe833a87dcb33af44
greinisch <greinisch@youwol.com>
2022-01-06 14:28:05 +0100

    :truck: move exceptions in dedicated file

--- youwol/context.py
+++ youwol/context.py
@@ -30,27 +30,6 @@ class MessageWebSocket(BaseModel):
     content: Union[Json, str]
 
 
-class CommandException(Exception):
-    def __init__(self, command: str, outputs: List[str]):
-        self.command = command
-        self.outputs = outputs
-        super().__init__(f"{self.command} failed")
-
-
-class ActionException(Exception):
-    def __init__(self, action: str, message: str):
-        self.action = action
-        self.message = message
-        super().__init__(self.message)
-
-
-class UserCodeException(Exception):
-    def __init__(self, message: str, tb: Any):
-        self.traceback = tb
-        self.message = message
-        super().__init__(self.message)
-
-
 async def log(
         level: LogLevel,
         text: Union[Json, str],
