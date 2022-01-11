5a37196fb0fe44815a45561defb1c3aa7009441d
greinisch <greinisch@youwol.com>
2022-01-11 10:11:10 +0100

    :coffin: no more admin/commands

--- youwol/routers/admin.py
+++ youwol/routers/admin.py
@@ -5,7 +5,6 @@ import youwol.routers.projects.router as projects
 import youwol.routers.environment.router as environment
 import youwol.routers.system.router as system
 import youwol.routers.local_cdn.router as local_cdn
-import youwol.routers.commands.router as commands
 import youwol.routers.custom_commands.router as custom_commands
 
 
@@ -20,7 +19,5 @@ router.include_router(projects.router, prefix=api_configuration.base_path+"/proj
                       tags=["projects"])
 router.include_router(local_cdn.router, prefix=api_configuration.base_path+"/local-cdn",
                       tags=["local-cdn"])
-router.include_router(commands.router, prefix=api_configuration.base_path+"/commands",
-                      tags=["commands"])
 router.include_router(custom_commands.router, prefix=api_configuration.base_path+"/custom-commands",
                       tags=["custom commands"])
