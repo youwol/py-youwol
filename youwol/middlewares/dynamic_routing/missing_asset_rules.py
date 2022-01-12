3cf6e57e1eaaa9a1f8d59a12a45e6b290bbf62b7
greinisch <greinisch@youwol.com>
2022-01-12 18:21:06 +0100

    :bug: dynamic-routing=>post-metadata if asset only in remote

--- youwol/middlewares/dynamic_routing/missing_asset_rules.py
+++ youwol/middlewares/dynamic_routing/missing_asset_rules.py
@@ -1,14 +1,16 @@
 from typing import Optional
 
-from youwol_utils.context import Context
-from youwol.environment.auto_download_thread import AssetDownloadThread
-from youwol.utils_low_level import redirect_api_remote
-
+from fastapi import HTTPException
 from starlette.middleware.base import RequestResponseEndpoint
 from starlette.requests import Request
 from starlette.responses import Response
 
+from youwol.environment.auto_download_thread import AssetDownloadThread
+from youwol.environment.clients import LocalClients
+from youwol.environment.forward_declaration import YouwolEnvironment
 from youwol.middlewares.models_dispatch import AbstractDispatch
+from youwol.utils_low_level import redirect_api_remote
+from youwol_utils.context import Context
 
 
 class GetRawDispatch(AbstractDispatch):
@@ -52,10 +54,32 @@ class PostMetadataDispatch(AbstractDispatch):
                     context: Context
                     ) -> Optional[Response]:
 
-        if not(request.method == "POST" and '/api/assets-gateway/assets/' in request.url.path):
+        if not (request.method == "POST" and '/api/assets-gateway/assets/' in request.url.path):
             return None
 
-        resp = await call_next(request)
-        # One other option would be to sync remote asset metadata:
-        # return await redirect_api_remote(request, context=context) if resp.status_code == 404 else resp
-        return resp
+        env = await context.get('env', YouwolEnvironment)
+        asset_id = request.url.path.split('/api/assets-gateway/assets/')[1].split('/')[0]
+
+        try:
+            # 'assets' and not 'assets_gateway' client is used such that following request won't be intercepted by
+            # dynamic_routing middlewares.
+            _resp = await LocalClients.get_assets_client(env).get(asset_id=asset_id)
+            # If the asset is in local: we only update the local version
+            return await call_next(request)
+        except HTTPException as e:
+            if e.status_code != 404:
+                raise e
+
+        resp_remote = await redirect_api_remote(request)
+
+        if resp_remote.status_code == 404:
+            raise HTTPException(
+                status_code=404,
+                detail="The asset seems to not exist neither in remote or local envs."
+            )
+        if resp_remote.status_code != 200:
+            raise HTTPException(
+                status_code=resp_remote.status_code,
+                detail="An error occurred while fetching the asset in remote env."
+            )
+        return resp_remote
