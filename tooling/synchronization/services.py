aa886da594c2089e7c0b14df6be9e80ad286bc7d
greinisch <greinisch@youwol.com>
2022-01-17 14:55:38 +0100

    :truck: synchronization=>sync with new backends location

--- tooling/synchronization/services.py
+++ tooling/synchronization/services.py
@@ -18,44 +18,44 @@ class ServiceInjection(NamedTuple):
 def included_services(platform_path, open_source_path):
 
     src_backend_services = platform_path / 'services'
-    dst_services = open_source_path / 'python' / 'py-youwol' / 'youwol' / 'services'
+    dst_services = open_source_path / 'python' / 'py-youwol' / 'youwol' / 'backends'
 
     return [
         ServiceInjection(
             src=src_backend_services / 'cdn-backend' / 'src' / 'youwol_cdn',
-            dst=dst_services / 'backs' / 'cdn',
+            dst=dst_services / 'cdn',
             include=["/__init__.py", "/models.py", "/resources_initialization.py", "/root_paths.py", "/utils*"]
             ),
         ServiceInjection(
             src=src_backend_services / 'treedb-backend' / 'src' / 'youwol_treedb',
-            dst=dst_services / 'backs' / 'treedb',
+            dst=dst_services / 'treedb',
             include=["/__init__.py", "/models.py", "/root_paths.py", "/utils.py"]
             ),
         ServiceInjection(
             src=src_backend_services / 'assets-backend' / 'src' / 'youwol_assets',
-            dst=dst_services / 'backs' / 'assets',
+            dst=dst_services / 'assets',
             include=["/__init__.py", "/models.py", "/root_paths.py", "/utils.py"]
             ),
         ServiceInjection(
             src=src_backend_services / 'flux-backend' / 'src' / 'youwol_flux',
-            dst=dst_services / 'backs' / 'flux',
+            dst=dst_services / 'flux',
             include=["/__init__.py", "/models.py", "/root_paths.py", "/suggestions.py", "/utils.py",
                      "/workflow_new_project.py", "/backward_compatibility.py"]
             ),
         ServiceInjection(
             src=src_backend_services / 'assets-gateway' / 'src' / 'youwol_assets_gateway',
-            dst=dst_services / 'backs' / 'assets_gateway',
+            dst=dst_services / 'assets_gateway',
             include=["/__init__.py", "/models.py", "/root_paths.py", "/package_drive.py", "/utils.py",
                      "/all_icons_emojipedia.py", "/raw_stores/*", "/routers/*"]
             ),
         ServiceInjection(
             src=src_backend_services / 'stories-backend' / 'src' / 'youwol_stories',
-            dst=dst_services / 'backs' / 'stories',
+            dst=dst_services / 'stories',
             include=["/__init__.py", "/models.py", "/root_paths.py", "/utils.py", "/all_icons_emojipedia.py"]
             ),
         ServiceInjection(
             src=src_backend_services / 'cdn-apps-server' / 'src' / 'youwol_cdn_apps_server',
-            dst=dst_services / 'backs' / 'cdn_apps_server',
+            dst=dst_services / 'cdn_apps_server',
             include=["/__init__.py", "/root_paths.py"]
             )
         ]
