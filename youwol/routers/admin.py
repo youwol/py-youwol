3d0ef43855bbae44e03c9976c2e9d21678f5fbbd
greinisch <greinisch@youwol.com>
2022-01-21 15:40:40 +0100

    :sparkles: /admin/ws -> add websocket for admin

--- youwol/routers/admin.py
+++ youwol/routers/admin.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocket
 
 import youwol.routers.authorization as authorization
 import youwol.routers.custom_commands.router as custom_commands
@@ -7,19 +7,29 @@ import youwol.routers.local_cdn.router as local_cdn
 import youwol.routers.projects.router as projects
 import youwol.routers.system.router as system
 from youwol.environment.youwol_environment import api_configuration
+from youwol.utils.utils_low_level import start_web_socket
+from youwol.web_socket import WebSocketsStore
 
 router = APIRouter()
 
 
-router.include_router(system.router, prefix=api_configuration.base_path+"/system",
+@router.websocket(api_configuration.base_path + "/ws")
+async def ws_endpoint(ws: WebSocket):
+    await ws.accept()
+    WebSocketsStore.adminChannel = ws
+    await ws.send_json({})
+    await start_web_socket(ws)
+
+
+router.include_router(system.router, prefix=api_configuration.base_path + "/system",
                       tags=["system"])
-router.include_router(environment.router, prefix=api_configuration.base_path+"/environment",
+router.include_router(environment.router, prefix=api_configuration.base_path + "/environment",
                       tags=["environment"])
-router.include_router(projects.router, prefix=api_configuration.base_path+"/projects",
+router.include_router(projects.router, prefix=api_configuration.base_path + "/projects",
                       tags=["projects"])
-router.include_router(local_cdn.router, prefix=api_configuration.base_path+"/local-cdn",
+router.include_router(local_cdn.router, prefix=api_configuration.base_path + "/local-cdn",
                       tags=["local-cdn"])
-router.include_router(custom_commands.router, prefix=api_configuration.base_path+"/custom-commands",
+router.include_router(custom_commands.router, prefix=api_configuration.base_path + "/custom-commands",
                       tags=["custom commands"])
 router.include_router(authorization.router, prefix=api_configuration.base_path+"/authorization",
                       tags=["authorization"])
