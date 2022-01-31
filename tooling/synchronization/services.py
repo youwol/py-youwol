f9b77135e586d43e0b1840af0096450ea2bd9685
greinisch <greinisch@youwol.com>
2022-01-31 09:40:43 +0100

    :wrench: update tooling & backends original path

--- tooling/synchronization/services.py
+++ tooling/synchronization/services.py
@@ -18,30 +18,31 @@ class ServiceInjection(NamedTuple):
 def included_services(platform_path, open_source_path):
 
     src_backend_services = platform_path / 'services'
+
     dst_services = open_source_path / 'python' / 'py-youwol' / 'youwol' / 'backends'
 
     return [
         ServiceInjection(
-            src=src_backend_services / 'cdn-backend' / 'src' / 'youwol_cdn',
+            src=open_source_path / 'python' / 'cdn-backend' / 'src' / 'youwol_cdn',
             dst=dst_services / 'cdn',
             include=["/__init__.py", "/models.py", "/resources_initialization.py", "/root_paths.py", "/utils*"]
-            ),
+        ),
         ServiceInjection(
             src=src_backend_services / 'treedb-backend' / 'src' / 'youwol_treedb',
             dst=dst_services / 'treedb',
             include=["/__init__.py", "/models.py", "/root_paths.py", "/utils.py"]
-            ),
+        ),
         ServiceInjection(
             src=src_backend_services / 'assets-backend' / 'src' / 'youwol_assets',
             dst=dst_services / 'assets',
             include=["/__init__.py", "/models.py", "/root_paths.py", "/utils.py"]
-            ),
+        ),
         ServiceInjection(
             src=src_backend_services / 'flux-backend' / 'src' / 'youwol_flux',
             dst=dst_services / 'flux',
             include=["/__init__.py", "/models.py", "/root_paths.py", "/suggestions.py", "/utils.py",
                      "/workflow_new_project.py", "/backward_compatibility.py"]
-            ),
+        ),
         ServiceInjection(
             src=src_backend_services / 'assets-gateway' / 'src' / 'youwol_assets_gateway',
             dst=dst_services / 'assets_gateway',
@@ -54,12 +55,12 @@ def included_services(platform_path, open_source_path):
             include=["/__init__.py", "/models.py", "/root_paths.py", "/utils.py", "/all_icons_emojipedia.py"]
         ),
         ServiceInjection(
-            src=src_backend_services / 'cdn-apps-server' / 'src' / 'youwol_cdn_apps_server',
+            src=open_source_path / 'python' / 'cdn-apps-server' / 'src' / 'youwol_cdn_apps_server',
             dst=dst_services / 'cdn_apps_server',
             include=["/__init__.py", "/root_paths.py"]
         ),
         ServiceInjection(
-            src=src_backend_services / 'cdn-sessions-storage' / 'src' / 'cdn_sessions_storage',
+            src=open_source_path / 'python' / 'cdn-sessions-storage' / 'src' / 'cdn_sessions_storage',
             dst=dst_services / 'cdn_sessions_storage',
             include=["/__init__.py", "/root_paths.py", "/utils.py"]
         )
