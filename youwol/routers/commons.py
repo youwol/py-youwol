6f257526a2768bd8847770128a1f358cc9368dde
greinisch <greinisch@youwol.com>
2022-02-09 17:30:07 +0100

    🔊 routers.commons => propagate ctx

--- youwol/routers/commons.py
+++ youwol/routers/commons.py
@@ -18,47 +18,49 @@ class Label(Enum):
     PIPELINE_STEP_RUNNING = "PIPELINE_STEP_RUNNING"
 
 
-async def ensure_path(path_item: PathResponse, assets_gateway_client: AssetsGatewayClient):
-
-    folders = path_item.folders
-    try:
-        if folders:
-            await assets_gateway_client.get_tree_folder(folder_id=folders[-1].folderId)
-        else:
-            await assets_gateway_client.get_tree_drive(drive_id=path_item.drive.driveId)
-    except HTTPException as e:
-        if e.status_code == 404:
-            if len(folders) <= 1:
-                await ensure_drive(path_item.drive, assets_gateway_client)
+async def ensure_path(path_item: PathResponse, assets_gateway_client: AssetsGatewayClient, context: Context):
+    async with context.start(action="ensure path") as ctx:
+        await context.info(text="target path", data=path_item)
+        folders = path_item.folders
+        try:
+            if folders:
+                await assets_gateway_client.get_tree_folder(folder_id=folders[-1].folderId, headers=ctx.headers())
             else:
-                await ensure_path(PathResponse(drive=path_item.drive, folders=folders[0:-1], item=path_item.item),
-                                  assets_gateway_client)
-            if not folders:
+                await assets_gateway_client.get_tree_drive(drive_id=path_item.drive.driveId, headers=ctx.headers())
+        except HTTPException as e:
+            if e.status_code == 404:
+                if len(folders) <= 1:
+                    await ensure_drive(drive=path_item.drive, assets_gateway_client=assets_gateway_client, context=ctx)
+                else:
+                    path_item = PathResponse(drive=path_item.drive, folders=folders[0:-1], item=path_item.item)
+                    await ensure_path(path_item=path_item, assets_gateway_client=assets_gateway_client, context=ctx)
+                if not folders:
+                    return
+                folder = folders[-1]
+                body = {"folderId": folder.folderId, "name": folder.name}
+                await assets_gateway_client.create_folder(parent_folder_id=folder.parentFolderId, body=body,
+                                                          headers=ctx.headers())
+
+
+async def ensure_drive(drive: DriveResponse, assets_gateway_client: AssetsGatewayClient, context: Context):
+    async with context.start(action="ensure drive", data=drive) as ctx:
+        try:
+            await assets_gateway_client.get_tree_drive(drive_id=drive.driveId, headers=ctx.headers())
+        except HTTPException as e:
+            if e.status_code == 404:
+                body = {"driveId": drive.driveId, "name": drive.name}
+                await assets_gateway_client.create_drive(group_id=drive.groupId, body=body, headers=ctx.headers())
                 return
-            folder = folders[-1]
-            body = {"folderId":  folder.folderId, "name": folder.name}
-            await assets_gateway_client.create_folder(parent_folder_id=folder.parentFolderId, body=body)
-
-
-async def ensure_drive(drive: DriveResponse,  assets_gateway_client: AssetsGatewayClient):
-
-    try:
-        await assets_gateway_client.get_tree_drive(drive_id=drive.driveId)
-    except HTTPException as e:
-        if e.status_code == 404:
-            body = {"driveId": drive.driveId, "name": drive.name}
-            await assets_gateway_client.create_drive(group_id=drive.groupId, body=body)
-            return
-        raise e
+            raise e
 
 
 async def local_path(tree_item: dict, context: Context):
     env = await context.get('env', YouwolEnvironment)
     treedb = LocalClients.get_treedb_client(env=env)
-    return await treedb.get_path(item_id=tree_item['treeId'])
+    return await treedb.get_path(item_id=tree_item['treeId'], headers=context.headers())
 
 
 async def remote_path(tree_item: dict, context: Context):
 
     treedb = await RemoteClients.get_treedb_client(context)
-    return await treedb.get_path(item_id=tree_item['treeId'])
+    return await treedb.get_path(item_id=tree_item['treeId'], headers=context.headers())
