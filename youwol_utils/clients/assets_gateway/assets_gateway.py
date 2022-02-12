4b9c2a7eb079c183ad24e63de046b8e32ab2cdc6
greinisch <greinisch@youwol.com>
2022-02-12 12:13:57 +0100

    ✨ youwol_utils assets_gtw => add 'delete_tree_item'

--- youwol_utils/clients/assets_gateway/assets_gateway.py
+++ youwol_utils/clients/assets_gateway/assets_gateway.py
@@ -84,6 +84,16 @@ class AssetsGatewayClient:
                     return await resp.json()
                 await raise_exception_from_response(resp)
 
+    async def delete_tree_item(self, item_id: str, **kwargs):
+
+        url = f"{self.url_base}/tree/items/{item_id}"
+
+        async with aiohttp.ClientSession(connector=self.get_aiohttp_connector(), headers=self.headers) as session:
+            async with await session.delete(url=url, **kwargs) as resp:
+                if resp.status == 200:
+                    return await resp.json()
+                await raise_exception_from_response(resp)
+
     async def get_tree_items_by_related_id(self, related_id: str, **kwargs):
 
         url = f"{self.url_base}/tree/items/from-related/{related_id}"
@@ -172,12 +182,21 @@ class AssetsGatewayClient:
                     return await resp.json()
                 await raise_exception_from_response(resp)
 
+    async def purge_drive(self, drive_id: str, **kwargs):
+
+        url = f"{self.url_base}/tree/drives/{drive_id}/purge"
+        async with aiohttp.ClientSession(connector=self.get_aiohttp_connector(), headers=self.headers) as session:
+            async with await session.delete(url=url, **kwargs) as resp:
+                if resp.status == 200:
+                    return await resp.json()
+                await raise_exception_from_response(resp)
+
     async def get_groups(self, **kwargs):
 
         url = f"{self.url_base}/groups"
 
         async with aiohttp.ClientSession(connector=self.get_aiohttp_connector(), headers=self.headers) as session:
-            async with await session.get(url=url,  **kwargs) as resp:
+            async with await session.get(url=url, **kwargs) as resp:
                 if resp.status == 200:
                     return await resp.json()
                 await raise_exception_from_response(resp)
