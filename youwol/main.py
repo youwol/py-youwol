384af98588ac624a0b099a126c2aef63e35405f4
Julien Décharne <jdecharne@youwol.com>
2022-03-11 16:46:30 +0100

    catch all exceptions on startup

--- youwol/main.py
+++ youwol/main.py
@@ -6,7 +6,6 @@ import daemon
 import lockfile
 import uvicorn
 
-from youwol.configuration.configuration_validation import ConfigurationLoadingException
 from youwol.environment.youwol_environment import YouwolEnvironmentFactory, print_invite, \
     YouwolEnvironment
 from youwol.fastapi_app import download_thread, fastapi_app
@@ -18,12 +17,14 @@ def main():
     assert_python()
     shutdown_script_path = Path().cwd() / "py-youwol.shutdown.sh"
     uvicorn_log_level = 'info' if get_main_arguments().verbose else 'critical'
+    print("Starting")
     try:
         env: YouwolEnvironment = asyncio.run(YouwolEnvironmentFactory.get())
         download_thread.go(env)
         print_invite(conf=env, shutdown_script_path=shutdown_script_path if get_main_arguments().daemonize else None)
 
         if get_main_arguments().daemonize:
+            print("Daemonizing")
             with daemon.DaemonContext(pidfile=lockfile.FileLock("py-youwol")):
                 shutdown_script_path.write_text(shutdown_daemon_script(pid=os.getpid()))
                 # app: incorrect type. More here: https://github.com/tiangolo/fastapi/issues/3927
@@ -33,7 +34,7 @@ def main():
             # app: incorrect type. More here: https://github.com/tiangolo/fastapi/issues/3927
             # noinspection PyTypeChecker
             uvicorn.run(fastapi_app, host="localhost", port=env.httpPort, log_level=uvicorn_log_level)
-    except ConfigurationLoadingException as e:
+    except Exception as e:
         print(e)
         exit()
     finally:
