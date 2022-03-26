ea4415917b64e94413bac6532b27b2b9a41f0f2c
greinisch <greinisch@youwol.com>
2022-03-26 09:51:55 +0100

    🔊 upload assets => propagate ctx.headers

--- youwol/routers/environment/upload_assets/flux_project.py
+++ youwol/routers/environment/upload_assets/flux_project.py
@@ -5,6 +5,7 @@ from youwol.environment.clients import RemoteClients, LocalClients
 from youwol.environment.youwol_environment import YouwolEnvironment
 from youwol.routers.environment.upload_assets.models import UploadTask
 from youwol_utils import JSON
+from youwol_utils.context import Context
 
 
 @dataclass
@@ -17,15 +18,19 @@ class UploadFluxProjectTask(UploadTask):
         return data
 
     async def create_raw(self, data: JSON, folder_id: str):
-        data['projectId'] = self.raw_id
-        remote_gtw = await RemoteClients.get_assets_gateway_client(context=self.context)
-        await remote_gtw.put_asset_with_raw(
-            kind='flux-project',
-            folder_id=folder_id,
-            data=json.dumps(data).encode()
-            )
+
+        async with self.context.start("UploadFluxProjectTask.create_raw") as ctx:  # type: Context
+            data['projectId'] = self.raw_id
+            remote_gtw = await RemoteClients.get_assets_gateway_client(context=ctx)
+            await remote_gtw.put_asset_with_raw(
+                kind='flux-project',
+                folder_id=folder_id,
+                data=json.dumps(data).encode(),
+                headers=ctx.headers()
+                )
 
     async def update_raw(self, data: JSON, folder_id: str):
         # <!> flux_client will be removed as it should not be available
-        flux_client = await RemoteClients.get_flux_client(context=self.context)
-        await flux_client.update_project(project_id=self.raw_id, body=data)
+        async with self.context.start("UploadFluxProjectTask.update_raw") as ctx:  # type: Context
+            flux_client = await RemoteClients.get_flux_client(context=ctx)
+            await flux_client.update_project(project_id=self.raw_id, body=data, headers=ctx.headers())
