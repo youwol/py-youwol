2a2efe24d83899be167408dfd482dd8c57f952e3
greinisch <greinisch@youwol.com>
2022-03-29 07:20:00 +0200

    ✨ youwol & youwol_utils => add assets-gtw pseudo lib

--- youwol/routers/native_backends.py
+++ youwol/routers/native_backends.py
@@ -17,10 +17,14 @@ cached_headers = None
 cdn.Dependencies.get_configuration = get_cdn_config
 router.include_router(cdn.router, prefix="/api/cdn-backend", tags=["cdn"])
 
+
+assets_gtw.Dependencies.get_configuration = get_assets_gtw_config
+router.include_router(assets_gtw.router, prefix="/api/assets-gateway", tags=["assets-gateway"])
+
+
 router.include_router(cdn_apps_server.router, prefix="/applications", tags=["cdn applications server"])
 router.include_router(treedb.router, prefix="/api/treedb-backend", tags=["treedb"])
 router.include_router(assets.router, prefix="/api/assets-backend", tags=["assets"])
 router.include_router(flux.router, prefix="/api/flux-backend", tags=["flux"])
 router.include_router(stories.router, prefix="/api/stories-backend", tags=["stories"])
-router.include_router(assets_gateway.router, prefix="/api/assets-gateway", tags=["assets-gateway"])
 router.include_router(cdn_sessions_storage.router, prefix="/api/cdn-sessions-storage", tags=["cdn-sessions-storage"])
---
+++ youwol_assets_gateway/configurations.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass
+from typing import Callable, Union, Type, Awaitable
+
+from youwol_utils import CdnClient
+from youwol_utils.clients.assets.assets import AssetsClient
+from youwol_utils.clients.data_api.data import (
+    DataClient, FILES_TABLE
+)
+from youwol_utils.clients.flux.flux import FluxClient
+from youwol_utils.clients.stories.stories import StoriesClient
+from youwol_utils.clients.treedb.treedb import TreeDbClient
+from youwol_utils.context import ContextLogger
+from youwol_utils.middlewares import Middleware
+from youwol_utils.middlewares.authentication_local import AuthLocalMiddleware
+from youwol_assets_gateway.raw_stores.data import DataStore
+from youwol_assets_gateway.raw_stores.flux_project import FluxProjectsStore
+from youwol_assets_gateway.raw_stores.package import PackagesStore
+from youwol_assets_gateway.raw_stores.story import StoriesStore
+
+AuthMiddleware = Union[Type[Middleware], Type[AuthLocalMiddleware]]
+
+
+@dataclass(frozen=True)
+class Constants:
+    cache_prefix: str = "assets-gateway"
+    unprotected_paths: Callable[[str], bool] = lambda url: \
+        url.path.split("/")[-1] == "healthz" or url.path.split("/")[-1] == "openapi-docs"
+
+    table_data_schema = FILES_TABLE
+
+
+@dataclass(frozen=True)
+class Configuration:
+    data_client: DataClient
+    flux_client: FluxClient
+    cdn_client: CdnClient
+    stories_client: StoriesClient
+    treedb_client: TreeDbClient
+    assets_client: AssetsClient
+
+    def assets_stores(self):
+        return [
+            FluxProjectsStore(client=self.flux_client),
+            PackagesStore(client=self.cdn_client),
+            DataStore(client=self.data_client),
+            StoriesStore(client=self.stories_client)
+        ]
+
+    ctx_logger: ContextLogger
+    admin_headers = None
+
+
+class Dependencies:
+    get_configuration: Callable[[], Awaitable[Configuration]]
+
+
+async def get_configuration():
+    return await Dependencies.get_configuration()
