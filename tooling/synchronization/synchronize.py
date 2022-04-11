bc08d5167bb10325ba366a47702a95d90416b110
greinisch <greinisch@youwol.com>
2022-04-11 16:06:14 +0200

    🔥 synchronization of backends => not needed anymore

--- tooling/synchronization/synchronize.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-from tooling.synchronization.services import sync_services
-
-sync_services(src_backend_services=Path("/home/greinisch/Projects/youwol-open-source/python"))
