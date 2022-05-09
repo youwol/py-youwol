7cca21df94af0adc8d6427cf435f899592373f7e
greinisch <greinisch@youwol.com>
2022-05-09 15:20:16 +0200

    ♻️ youwol_utils.clients.local_fs => replace xattr for metadata by actual files

--- youwol_utils/clients/file_system/local_file_system.py
+++ youwol_utils/clients/file_system/local_file_system.py
@@ -8,6 +8,7 @@ from fastapi import HTTPException
 
 from youwol_utils import create_dir_if_needed
 from youwol_utils.clients.file_system.interfaces import FileSystemInterface
+from youwol_utils.utils_paths import write_json, parse_json
 
 
 @dataclass(frozen=True)
@@ -26,44 +27,44 @@ class LocalFileSystem(FileSystemInterface):
         create_dir_if_needed(path)
         content = data.read()
         path.open('wb').write(content)
-        for key, value in metadata.items():
-            os.setxattr(path, f'user.{key}', value.encode('utf8'))
+
+        path_metadata = self.get_full_path(f"{object_name}.metadata.json")
+        write_json(metadata, path_metadata)
 
     async def get_info(self, object_name: str, **kwargs):
 
-        path = self.get_full_path(object_name)
-        if not path.exists():
-            raise HTTPException(status_code=404, detail=f"LocalFileSystem.get_stats: Object '{path}' "
-                                                        f"not found")
+        self.ensure_object_exist(object_name)
+        path_metadata = self.get_full_path(f"{object_name}.metadata.json")
+        if not path_metadata.exists():
+            return {"metadata": {}}
 
-        metadata = {}
-        for key in ['contentType', 'contentEncoding', 'fileName', 'fileId']:
-            metadata[key] = os.getxattr(path, f'user.{key}').decode('utf8')
-
-        return {"metadata": metadata}
+        return {"metadata": parse_json(path_metadata)}
 
     async def set_metadata(self, object_name: str, metadata: Dict[str, str], **kwargs):
 
-        path = self.get_full_path(object_name)
-        if not path.exists():
-            raise HTTPException(status_code=404, detail=f"LocalFileSystem.set_metadata: Object '{path}'"
-                                                        f" not found")
-
-        for key, value in metadata.items():
-            os.setxattr(path, f'user.{key}', value.encode('utf8'))
+        self.ensure_object_exist(object_name)
+        path_metadata = self.get_full_path(f"{object_name}.metadata.json")
+        write_json(metadata, path_metadata)
 
     async def get_object(self, object_name: str, **kwargs):
-        path = self.get_full_path(object_name)
+
+        path = self.ensure_object_exist(object_name)
         if not path.exists():
-            raise HTTPException(status_code=404, detail=f"LocalFileSystem.get_object: Object '{path}' "
-                                                        f"not found")
+            raise HTTPException(status_code=404, detail=f"LocalFileSystem.get_object: Object '{path}' not found")
 
         return path.read_bytes()
 
     async def remove_object(self, object_name: str, **kwargs):
-        path = self.get_full_path(object_name)
-        if not path.exists():
-            raise HTTPException(status_code=404, detail=f"LocalFileSystem.remove_object: Object '{path}'"
-                                                        f" not found")
+
+        path = self.ensure_object_exist(object_name)
 
         os.remove(path)
+        path_metadata = self.get_full_path(f"{object_name}.metadata.json")
+        if path_metadata.exists():
+            os.remove(path_metadata)
+
+    def ensure_object_exist(self, object_name: str):
+        path = self.get_full_path(object_name)
+        if not path.exists():
+            raise HTTPException(status_code=404, detail=f"LocalFileSystem.remove_object: Object '{path}' not found")
+        return path
