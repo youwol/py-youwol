876e44bf3258b6f3b197988939e340801cc8a693
greinisch <greinisch@youwol.com>
2022-06-08 08:51:49 +0200

    ♻️ youwol_utils.file_system => export 'file_system'

--- youwol_utils/clients/__init__.py
+++ youwol_utils/clients/__init__.py
@@ -1,6 +1,7 @@
 from .auth import *
 from .cache import *
 from .cdn import *
+from .file_system import *
 from .docdb import *
 from .storage import *
 from .types import *
--- youwol_utils/clients/file_system/__init__.py
+++ youwol_utils/clients/file_system/__init__.py
@@ -1,2 +1,3 @@
 from .interfaces import *
 from .local_file_system import *
+from .minio_file_system import *
