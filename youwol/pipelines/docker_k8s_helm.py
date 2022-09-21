8231a8776ab55fce5a37a854748738acd8e4a66a
youwol-guillaume <greinisch@youwol.com>
2022-09-21 16:13:23 +0200

    :recycle: InstallHelmStep => handle the case of multiple K8sDeployment

--- youwol/pipelines/docker_k8s_helm.py
+++ youwol/pipelines/docker_k8s_helm.py
@@ -1,16 +1,17 @@
 import os
 from pathlib import Path
-from typing import Callable, Union, List
+from typing import Callable, Union, List, Optional
 
 from pydantic import BaseModel
-
+from kubernetes_asyncio import config as k8s_config
 from youwol.configuration.models_k8s import DockerRepo
-from youwol.environment.models_project import PipelineStep, Project, ExplicitNone, FlowId, RunImplicit
+from youwol.environment.models_project import PipelineStep, Project, ExplicitNone, FlowId, RunImplicit, Manifest, \
+    PipelineStepStatus, FileListing
 from youwol.exceptions import CommandException
 from youwol.pipelines.deploy_service import HelmPackage
 from youwol.utils.k8s_utils import get_cluster_info
 from youwol_utils.context import Context
-from youwol_utils.utils_paths import parse_yaml, FileListing
+from youwol_utils.utils_paths import parse_yaml
 
 
 class PublishDockerStepConfig(BaseModel):
@@ -74,43 +75,75 @@ class InstallHelmStepConfig(BaseModel):
     valuesPath: Callable[[Project, Context], dict]
 
 
+def get_helm_package(config: InstallHelmStepConfig, project: Project, context: Context):
+
+    with_values = config.overridingHelmValues(project, context) if config.overridingHelmValues else {}
+    chart_path = project.path / "chart"
+
+    helm_package = HelmPackage(
+        name=project.name,
+        namespace=config.namespace,
+        chart_folder=chart_path,
+        with_values=with_values,
+        values_filename='values.yaml',
+        secrets=config.secrets,
+        chart_explorer=get_chart_explorer(chart_path)
+    )
+    return helm_package
+
+
+install_helm_running = False
+
+
 class InstallHelmStep(PipelineStep):
+    isRunning: bool = False
     id = 'install-helm'
     config: InstallHelmStepConfig
-
+    k8sContext: str
     run: ExplicitNone = ExplicitNone()
 
     async def get_helm_package(self, project: Project, context: Context):
 
-        with_values = self.config.overridingHelmValues(project, context) if self.config.overridingHelmValues else {}
-        chart_path = project.path / "chart"
-
-        helm_package = HelmPackage(
-            name=project.name,
-            namespace=self.config.namespace,
-            chart_folder=chart_path,
-            with_values=with_values,
-            values_filename='values.yaml',
-            secrets=self.config.secrets,
-            chart_explorer=get_chart_explorer(chart_path)
-        )
+        helm_package = get_helm_package(config=self.config, project=project, context=context)
         await context.send(data=helm_package)
         return helm_package
 
     async def execute_run(self, project: Project, flow_id: FlowId, context: Context):
-
+        global install_helm_running
+        if install_helm_running:
+            raise CommandException(command="Deploy helm chart",
+                                   outputs=["Helm is already installing a chart, this step can not be parallelize"])
         outputs = []
-        await context.info(text="")
+
+        def on_enter(_: Context):
+            global install_helm_running
+            install_helm_running = True
+
+        def on_exit(_):
+            global install_helm_running
+            install_helm_running = False
+
         async with context.start(
                 action="InstallHelmStep.execute_run",
+                on_enter=on_enter,
+                on_exit=on_exit,
                 with_attributes={
                     "namespace": self.config.namespace
                 }) as ctx:
+            await k8s_config.load_kube_config(context=self.k8sContext)
+            stream = os.popen(f"kubectl config use-context {self.k8sContext}")
 
             k8s_info = await get_cluster_info()
             if not k8s_info:
                 outputs.append("Can not connect to k8s proxy")
                 raise CommandException(command="Deploy helm chart", outputs=outputs)
+            await ctx.info(
+                text="k8s_info",
+                data={
+                    "k8s context": stream.read(),
+                    "k8s info": k8s_info
+                }
+            )
 
             helm_package = await self.get_helm_package(project=project, context=ctx)
 
@@ -139,12 +172,32 @@ class InstallHelmStep(PipelineStep):
 
         return outputs
 
+    async def get_status(self, project: Project, flow_id: str,
+                         last_manifest: Optional[Manifest], context: Context) -> PipelineStepStatus:
 
-class InstallDryRunHelmStep(InstallHelmStep):
+        async with context.start(
+                action="InstallHelmStep.get_status",
+                with_attributes={
+                    "namespace": self.config.namespace
+                }) as ctx:
+
+            stream = os.popen(f"( kubectl config use-context {self.k8sContext} "
+                              f"&& helm get manifest {project.name} -n {self.config.namespace})")
+            outputs = stream.read()
+            await ctx.info("retrieved helm manifest", data={"lines": outputs})
+            version_output = next(output for output in outputs.split('\n') if 'app.kubernetes.io/version:' in output)
+            version = version_output.split('app.kubernetes.io/version: "')[1][0:-1]
+            await ctx.info(f"found deployed chart @version {version}")
+            return PipelineStepStatus.OK if version == project.version else PipelineStepStatus.outdated
+
+
+class InstallDryRunHelmStep(PipelineStep):
     id = 'dry-run-helm'
     config: InstallHelmStepConfig
 
     run: ExplicitNone = ExplicitNone()
+
+    sources: FileListing = FileListing(include=["chart"])
 
     async def execute_run(self, project: Project, flow_id: FlowId, context: Context):
 
@@ -156,8 +209,7 @@ class InstallDryRunHelmStep(InstallHelmStep):
                     "namespace": self.config.namespace
                 }) as ctx:
 
-            helm_package = await self.get_helm_package(project=project,
-                                                       context=ctx)
+            helm_package = get_helm_package(config=self.config, project=project, context=context)
 
             return_code, cmd, outputs_bash = await helm_package.dry_run(context=ctx)
             outputs = outputs + [cmd] + outputs_bash
