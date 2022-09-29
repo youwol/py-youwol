c0f94b4134bbec6ef39da4b3c4d7957d4295e226
youwol-guillaume <greinisch@youwol.com>
2022-09-29 10:35:59 +0200

    :recycle: pipeline ts webpack PublishNpmStep => move code in dedicated file

---
+++ youwol/pipelines/pipeline_typescript_weback_npm/common/npm_step.py
@@ -0,0 +1,80 @@
+import asyncio
+import functools
+import datetime
+from typing import Optional
+
+
+from youwol.environment.models_project import PipelineStep, Project, Manifest, PipelineStepStatus, FlowId
+from youwol.exceptions import CommandException
+from youwol.pipelines.pipeline_typescript_weback_npm.common import NpmRepo
+from youwol.utils.utils_low_level import execute_shell_cmd
+from youwol_utils.context import Context
+
+
+async def get_shasum_published(project: Project, context: Context):
+    exit_code, outputs = await execute_shell_cmd(cmd=f'npm view {project.name}@{project.version} dist.shasum',
+                                                 context=context)
+    return outputs[0].replace('\n', '')
+
+
+async def get_shasum_local(project: Project, context: Context):
+    shasum_prefix = 'shasum:'
+    exit_code, outputs = await execute_shell_cmd(cmd=f'(cd {project.path} && npm publish --dry-run)',
+                                                 context=context)
+    shasum_line = next(line for line in outputs if shasum_prefix in line)
+    return shasum_line.split(shasum_prefix)[1].strip()
+
+
+class PublishNpmStep(PipelineStep):
+    id: str = "publish-npm"
+    run: str = "yarn publish --access public"
+    npm_target: NpmRepo
+
+    async def execute_run(self, project: 'Project', flow_id: FlowId, context: Context):
+
+        async with context.start(
+                action="PublishNpmStep.execute_run",
+        ) as ctx:
+            npm_outputs = await self.npm_target.publish(project=project, context=ctx)
+            shasum_published, shasum_local = await asyncio.gather(
+                get_shasum_published(project=project, context=context),
+                get_shasum_local(project=project, context=context)
+            )
+
+            return {
+                "npm_outputs": npm_outputs,
+                "shasum_published": shasum_published,
+                "shasum_local": shasum_local,
+                "version": project.version,
+                "date": f'{datetime.date.today()}'
+            }
+
+    async def get_status(self, project: Project, flow_id: str, last_manifest: Optional[Manifest], context: Context) \
+            -> PipelineStepStatus:
+
+        async with context.start(
+                action="PublishNpmStep.get_status",
+        ) as ctx:
+            shasum_prefix = 'shasum:'
+            cmd = f"npm view {project.name} versions --json"
+            exit_code, outputs = await execute_shell_cmd(cmd=cmd, context=ctx)
+
+            if exit_code != 0 and 'E404' in outputs[0]:
+                return PipelineStepStatus.none
+
+            if exit_code != 0:
+                raise CommandException(command=cmd, outputs=outputs)
+
+            flat_output = functools.reduce(lambda acc, e: acc+e, outputs, "")
+            if f'"{project.version}"' not in flat_output:
+                return PipelineStepStatus.none
+
+            exit_code, outputs = await execute_shell_cmd(cmd=f'npm view {project.name}@{project.version} dist.shasum',
+                                                         context=ctx)
+            shasum_published = outputs[0].replace('\n', '')
+            exit_code, outputs = await execute_shell_cmd(cmd=f'(cd {project.path} && npm publish --dry-run)',
+                                                         context=ctx)
+            shasum_line = next(line for line in outputs if shasum_prefix in line)
+            shasum_project = shasum_line.split(shasum_prefix)[1].strip()
+            return PipelineStepStatus.OK if shasum_published == shasum_project else PipelineStepStatus.outdated
+
--- youwol/pipelines/pipeline_typescript_weback_npm/common/utils.py
+++ youwol/pipelines/pipeline_typescript_weback_npm/common/utils.py
@@ -1,21 +1,18 @@
-import asyncio
 import functools
 import glob
 import shutil
-import datetime
 from pathlib import Path
-from typing import Dict, List, Union, Optional
+from typing import Dict, List, Union
 
 import pyparsing
 import semantic_version
 
-from youwol.environment.models_project import PipelineStep, Project, Manifest, PipelineStepStatus, FlowId
+from youwol.environment.models_project import PipelineStep
 from youwol.environment.youwol_environment import YouwolEnvironment
-from youwol.exceptions import CommandException
 from youwol.pipelines import PublishCdnRemoteStep, PackagesPublishYwCdn
-from youwol.pipelines.pipeline_typescript_weback_npm.common import NpmRepo
 from youwol.pipelines.pipeline_typescript_weback_npm.common import Template, PackageType, PackagesPublishNpm
-from youwol.utils.utils_low_level import sed_inplace, execute_shell_cmd
+from youwol.pipelines.pipeline_typescript_weback_npm.common.npm_step import PublishNpmStep
+from youwol.utils.utils_low_level import sed_inplace
 from youwol_cdn_backend import get_api_key
 from youwol_cdn_backend.loading_graph_implementation import exportedSymbols
 from youwol_utils import parse_json, write_json, JSON
@@ -161,69 +158,6 @@ def generate_webpack_config(source: Path, working_path: Path, input_template: Te
 
     if input_template.type == PackageType.Application:
         sed_inplace(filename, '"{{devServer.port}}"', str(input_template.devServer.port))
-
-
-async def get_shasum_published(project: Project, context: Context):
-    exit_code, outputs = await execute_shell_cmd(cmd=f'npm view {project.name}@{project.version} dist.shasum',
-                                                 context=context)
-    return outputs[0].replace('\n', '')
-
-
-async def get_shasum_local(project: Project, context: Context):
-    shasum_prefix = 'shasum:'
-    exit_code, outputs = await execute_shell_cmd(cmd=f'(cd {project.path} && npm publish --dry-run)',
-                                                 context=context)
-    shasum_line = next(line for line in outputs if shasum_prefix in line)
-    return shasum_line.split(shasum_prefix)[1].strip()
-
-
-class PublishNpmStep(PipelineStep):
-    id: str = "publish-npm"
-    run: str = "yarn publish --access public"
-    npm_target: NpmRepo
-
-    async def execute_run(self, project: 'Project', flow_id: FlowId, context: Context):
-
-        async with context.start(
-                action="PublishNpmStep.execute_run",
-        ) as ctx:
-            npm_outputs = await self.npm_target.publish(project=project, context=ctx)
-            shasum_published, shasum_local = await asyncio.gather(
-                get_shasum_published(project=project, context=context),
-                get_shasum_local(project=project, context=context)
-            )
-
-            return {
-                "npm_outputs": npm_outputs,
-                "shasum_published": shasum_published,
-                "shasum_local": shasum_local,
-                "version": project.version,
-                "date": f'{datetime.date.today()}'
-            }
-
-    async def get_status(self, project: Project, flow_id: str, last_manifest: Optional[Manifest], context: Context) \
-            -> PipelineStepStatus:
-
-        async with context.start(
-                action="PublishNpmStep.get_status",
-        ) as ctx:
-            shasum_prefix = 'shasum:'
-            cmd = f"npm view {project.name} versions --json"
-            exit_code, outputs = await execute_shell_cmd(cmd=cmd, context=ctx)
-            if exit_code != 0:
-                raise CommandException(command=cmd, outputs=outputs)
-            flat_output = functools.reduce(lambda acc, e: acc+e, outputs, "")
-            if f'"{project.version}"' not in flat_output:
-                return PipelineStepStatus.none
-
-            exit_code, outputs = await execute_shell_cmd(cmd=f'npm view {project.name}@{project.version} dist.shasum',
-                                                         context=ctx)
-            shasum_published = outputs[0].replace('\n', '')
-            exit_code, outputs = await execute_shell_cmd(cmd=f'(cd {project.path} && npm publish --dry-run)',
-                                                         context=ctx)
-            shasum_line = next(line for line in outputs if shasum_prefix in line)
-            shasum_project = shasum_line.split(shasum_prefix)[1].strip()
-            return PipelineStepStatus.OK if shasum_published == shasum_project else PipelineStepStatus.outdated
 
 
 async def create_sub_pipelines_publish(start_step: str, context: Context):
