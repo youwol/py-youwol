53252218ec7e56598748f1dd4522d4129a945ac2
youwol-guillaume <greinisch@youwol.com>
2022-11-26 13:01:54 +0100

    🚚 rename 'middlewares.auth_middleware.py' => 'middlewares.auth_jwt_provider.py'

--- youwol/middlewares/__init__.py
+++ youwol/middlewares/__init__.py
@@ -1,4 +1,4 @@
-from .auth_middleware import *
+from .auth_jwt_provider import *
 from .browser_caching_middleware import *
 from .hybridizer_middleware import *
 from .local_cloud_hybridizers import *
