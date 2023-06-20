fcba2718f37341df7d37cbaa3f9fcd2dbcf9f78a
Julien Décharne <jdecharne@youwol.com>
2023-06-20 08:50:53 +0200

    ✨ Update PATH with sys.prefix if necessary
    TG-1208 #closed

---
+++ src/youwol/app/wrapper.py
@@ -0,0 +1,23 @@
+# standard library
+import os
+import sys
+
+from pathlib import Path
+
+
+def ensure_venv_python_in_path():
+    python_bin_dir = Path(sys.prefix) / "bin"
+    if not any(path == str(python_bin_dir) for path in os.environ["PATH"].split(":")):
+        print(
+            f"Adding detected Python binaries directory '{python_bin_dir}' to environment variable PATH"
+        )
+        os.environ["PATH"] = f"{python_bin_dir}:{os.environ['PATH']}"
+
+
+def main():
+    ensure_venv_python_in_path()
+    os.execvp(file=sys.executable, args=["python", *sys.argv[1:]])
+
+
+if __name__ == "__main__":
+    main()
