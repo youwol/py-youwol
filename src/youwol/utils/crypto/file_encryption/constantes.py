4c54b72f80def4aee19d05675948b44a276e875b
Julien Décharne <jdecharne@youwol.com>
2024-01-24 10:28:40 +0100

    📝 `youwol.utils.crypto.file_encryption.constantes`
    TG-1924

--- src/youwol/utils/crypto/file_encryption/constantes.py
+++ src/youwol/utils/crypto/file_encryption/constantes.py
@@ -9,17 +9,21 @@ ALGO_HEADER_LENGTH = 1
 
 class Algo(Enum):
     """
-    Available algorithm for encryption.
+    Available algorithms for file encryption.
+
+    Enum entry value will be the first byte of the encrypted file, and identify the algorithm used.
     """
 
     NULL = 0x0A
     """
-    A single new line '\n'
+    A NULL cipher, an almost no-op encryption: encrypted data will be almost the same as plain data,
+     except for the addition of a single new line character at the begin of the file.
     """
 
     SIV_256 = 0x01
     """
-    SIV 256
+    AES-SIV (RFC 5297) with a key size of 256 bits : Synthetic Initialization Vector (SIV) mode
+     for AES cipher, with a block size of 128 bits.
     """
 
 
