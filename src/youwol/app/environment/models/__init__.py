08cb5154860536d337d5036eb2bab61e66b63966
youwol-guillaume <greinisch@youwol.com>
2024-01-31 14:19:43 +0100

    ♻️ [environment] => move `FlowSwitch` in dedicated module

--- src/youwol/app/environment/models/__init__.py
+++ src/youwol/app/environment/models/__init__.py
@@ -4,6 +4,7 @@ The models used in YouWolEnvironment and its Configuration.
 
 # relative
 from .defaults import *
+from .flow_switches import *
 from .models import *
 from .models_config import *
 from .predefined_configs import *
