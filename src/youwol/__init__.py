d88f8fc9f74af22d0bd65e903cd092735e895415
Nestor YouBot <nestor@youwol.com>
2024-04-08 09:00:23 +0000

    🔖 prepare for next version 0.1.9rc1
    version_management.py debug logging:
    original_version='0.1.9rc0'
    command='restore_dev'
    final_version_string='0.1.9rc1.dev'
    final_version_canonical='0.1.9rc1.dev0'

--- src/youwol/__init__.py
+++ src/youwol/__init__.py
@@ -18,4 +18,4 @@ javascript applications, typescript projects, etc.) using a specific stack.
 They formalize steps such as initialization, building, testing, and deployment.
 """
 
-__version__ = "0.1.9rc"
+__version__ = "0.1.9rc1.dev"
