d69bb6e0ba99a8bd8e4b0bacb94d75402d919aa9
youwol-guillaume <greinisch@youwol.com>
2024-09-17 11:24:45 +0200

    ✨ [yw_clients] Include `YouWolException` list

--- lib/yw_clients/yw_clients/http/exceptions.py
+++ lib/yw_clients/yw_clients/http/exceptions.py
@@ -3,6 +3,7 @@ from typing import Any
 
 # third parties
 from aiohttp import ClientResponse, ContentTypeError
+from pydantic import BaseModel
 from starlette.exceptions import HTTPException
 
 # Youwol clients
@@ -36,6 +37,231 @@ class YouWolException(HTTPException):
         return f"""{self.status_code} : {self.detail}"""
 
 
+class ServerError(YouWolException):
+    exceptionType = "ServerError"
+
+    def __init__(self, detail: Any, **kwargs):
+        YouWolException.__init__(self, status_code=500, detail=detail, **kwargs)
+        self.exceptionType = ServerError.exceptionType
+        self.detail = detail
+
+    def __str__(self):
+        return f"""{self.status_code} : {self.detail}"""
+
+
+class PublishPackageError(YouWolException):
+    exceptionType = "PublishPackageError"
+
+    def __init__(self, context: str, **kwargs):
+        YouWolException.__init__(
+            self, status_code=422, detail={"context": context}, **kwargs
+        )
+        self.exceptionType = PublishPackageError.exceptionType
+        self.context = context
+
+    def __str__(self):
+        return f"""The package can not be published: {self.context}"""
+
+
+class PackagesNotFound(YouWolException):
+    exceptionType = "PackagesNotFound"
+
+    def __init__(self, context: str, packages: list[str], **kwargs):
+        YouWolException.__init__(
+            self,
+            status_code=404,
+            detail={"context": context, "packages": packages},
+            **kwargs,
+        )
+        self.exceptionType = PackagesNotFound.exceptionType
+        self.packages = packages
+        self.context = context
+
+    def __str__(self):
+        return f"""Packages not found. Context: {self.context}; Packages: {self.packages}"""
+
+
+class IndirectPackagesNotFound(YouWolException):
+    exceptionType = "IndirectPackagesNotFound"
+
+    def __init__(self, context: str, paths: dict[str, list[str]], **kwargs):
+        YouWolException.__init__(
+            self, status_code=404, detail={"context": context, "paths": paths}, **kwargs
+        )
+        self.exceptionType = IndirectPackagesNotFound.exceptionType
+        self.paths = paths
+        self.context = context
+
+    def __str__(self):
+        return f"""Packages not found. Context: {self.context}; paths: {self.paths}"""
+
+
+class DependencyErrorData(BaseModel):
+    key: str
+    path: list[str]
+    detail: str
+
+
+class DependenciesError(YouWolException):
+    exceptionType = "DependenciesError"
+
+    def __init__(self, context: str, errors: list[dict[str, Any]], **kwargs):
+        """
+
+        :param context: context of the error
+        :param errors: An error as a dict like {'key':str, 'paths': List[str], 'detail': str}
+        :param kwargs: forwarding arguments to YouWolException
+        """
+        YouWolException.__init__(
+            self,
+            status_code=404,
+            detail={"context": context, "errors": errors},
+            **kwargs,
+        )
+        self.exceptionType = DependenciesError.exceptionType
+        self.context = context
+
+    def __str__(self):
+        return f"""Dependencies not found. Error: {self.detail}"""
+
+
+class CircularDependencies(YouWolException):
+    exceptionType = "CircularDependencies"
+
+    def __init__(self, context: str, packages: dict[str, list[AnyDict]], **kwargs):
+        YouWolException.__init__(
+            self,
+            status_code=404,
+            detail={"context": context, "packages": packages},
+            **kwargs,
+        )
+        self.exceptionType = CircularDependencies.exceptionType
+        self.packages = packages
+        self.context = context
+
+    def __str__(self):
+        return f"""Packages not found. Context: {self.context}; Packages: {self.packages}"""
+
+
+class ProjectNotFound(YouWolException):
+    exceptionType = "ProjectNotFound"
+
+    def __init__(self, context: str, project: str, **kwargs):
+        YouWolException.__init__(
+            self,
+            status_code=404,
+            detail={"context": context, "project": project},
+            **kwargs,
+        )
+        self.exceptionType = ProjectNotFound.exceptionType
+        self.project = project
+        self.context = context
+
+    def __str__(self):
+        return f"""Project '{self.project}' not found in workspace. Context: {self.context}"""
+
+
+class PipelineStepNotFound(YouWolException):
+    exceptionType = "PipelineStepNotFound"
+
+    def __init__(self, context: str, project: str, step: str, **kwargs):
+        YouWolException.__init__(
+            self,
+            status_code=404,
+            detail={"context": context, "project": project, "step": step},
+            **kwargs,
+        )
+        self.exceptionType = PipelineStepNotFound.exceptionType
+        self.project = project
+        self.step = step
+        self.context = context
+
+    def __str__(self):
+        return f"""Pipeline's step '{self.step}' not found in project '{self.project}'. Context: {self.context}"""
+
+
+class PipelineFlowNotFound(YouWolException):
+    exceptionType = "PipelineFlowNotFound"
+
+    def __init__(self, context: str, project: str, flow: str, **kwargs):
+        YouWolException.__init__(
+            self,
+            status_code=404,
+            detail={"context": context, "project": project, "flow": flow},
+            **kwargs,
+        )
+        self.exceptionType = PipelineFlowNotFound.exceptionType
+        self.project = project
+        self.flow = flow
+        self.context = context
+
+    def __str__(self):
+        return f"""Pipeline's flow '{self.flow}' not found in project '{self.project}'. Context: {self.context}"""
+
+
+class FolderNotFound(YouWolException):
+    exceptionType = "FolderNotFound"
+
+    def __init__(self, context: str, folder: str, **kwargs):
+        YouWolException.__init__(
+            self,
+            status_code=404,
+            detail={"context": context, "folder": folder},
+            **kwargs,
+        )
+        self.exceptionType = FolderNotFound.exceptionType
+        self.context = context
+        self.folder = folder
+
+    def __str__(self):
+        return f"""The folder '{self.folder}' is not found. Context: {self.context}"""
+
+
+class ResourcesNotFoundException(YouWolException):
+    exceptionType = "ResourcesNotFoundException"
+
+    def __init__(self, path: str, detail: str = "", **kwargs):
+        YouWolException.__init__(
+            self, status_code=404, detail={"path": path, "detail": detail}, **kwargs
+        )
+        self.path = path
+        self.exceptionType = ResourcesNotFoundException.exceptionType
+
+    def __str__(self):
+        return f"""The resource at path '{self.path}' is not a file."""
+
+
+class QueryIndexException(YouWolException):
+    exceptionType = "QueryIndexException"
+
+    def __init__(self, query: str, error: Any, **kwargs):
+        YouWolException.__init__(
+            self, status_code=404, detail={"query": query, "error": error}, **kwargs
+        )
+        self.query = query
+        self.error = error
+        self.exceptionType = QueryIndexException.exceptionType
+
+    def __str__(self):
+        return (
+            f"""The query '{self.query}' resolved to unexpected result: ${self.error}"""
+        )
+
+
+class InvalidInput(YouWolException):
+    exceptionType = "InvalidInput"
+
+    def __init__(self, error: str, **kwargs):
+        YouWolException.__init__(
+            self, status_code=422, detail={"error": error}, **kwargs
+        )
+        self.error = error
+        self.exceptionType = InvalidInput.exceptionType
+
+    def __str__(self):
+        return f"""Invalid input: {self.error}"""
+
+
 class UpstreamResponseException(YouWolException):
     """
     Represents an exception that has been generated from an HTTP call to a service.
@@ -48,8 +274,6 @@ class UpstreamResponseException(YouWolException):
     the `detail` attribute of the class presents a recursive structure that depicts the callstack of
     services from which the original exception initiated.
     """
-
-    ManagedExceptions: list[type[YouWolException]] = []
 
     exceptionType = "UpstreamResponseException"
 
@@ -84,6 +308,24 @@ class UpstreamResponseException(YouWolException):
         return """Upstream Exception"""
 
 
+YouwolExceptions: list[type[YouWolException]] = [
+    ServerError,
+    PipelineFlowNotFound,
+    FolderNotFound,
+    PipelineStepNotFound,
+    ProjectNotFound,
+    PublishPackageError,
+    PackagesNotFound,
+    IndirectPackagesNotFound,
+    DependenciesError,
+    CircularDependencies,
+    ResourcesNotFoundException,
+    QueryIndexException,
+    InvalidInput,
+    UpstreamResponseException,
+]
+
+
 async def upstream_exception_from_response(
     raw_resp: ClientResponse, **kwargs
 ) -> UpstreamResponseException:
@@ -102,7 +344,7 @@ async def upstream_exception_from_response(
             exception_type = next(
                 (
                     e
-                    for e in UpstreamResponseException.ManagedExceptions
+                    for e in YouwolExceptions
                     if e.exceptionType == resp["exceptionType"]
                 ),
                 None,
